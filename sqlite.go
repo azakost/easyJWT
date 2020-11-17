94e78ba936e356303efefa87285b32b04919d0e6
Azamat ALIMBAYEV <azakost@gmail.com>
2020-11-17 11:35:46 +0600

    feat: added select and db-check features

--- sqlite.go
+++ sqlite.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"io/ioutil"
 	"os"
+	"reflect"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -44,4 +45,47 @@ func Insert(query string, args ...interface{}) error {
 	}
 	err(tx.Commit())
 	return nil
+}
+
+func Select(model interface{}, query string, args ...interface{}) {
+	db, databaseError := sql.Open("sqlite3", DBname)
+	err(databaseError)
+	defer db.Close()
+	statement, stmError := db.Prepare(query)
+	err(stmError)
+	rows, rowsError := statement.Query(args...)
+	err(rowsError)
+	defer rows.Close()
+	container := reflect.Indirect(reflect.ValueOf(model))
+	v := container.Type().Elem()
+	len := v.NumField()
+	tmp := make([]interface{}, len)
+	dest := make([]interface{}, len)
+	for i := range tmp {
+		dest[i] = &tmp[i]
+	}
+	for rows.Next() {
+		scanError := rows.Scan(dest...)
+		err(scanError)
+		row := reflect.Indirect(reflect.New(v))
+		for i, t := range tmp {
+			f := row.Field(i)
+			f.Set(reflect.ValueOf(t))
+		}
+		container.Set(reflect.Append(container, row))
+	}
+}
+
+func InDB(query string, data interface{}) bool {
+	db, databaseError := sql.Open("sqlite3", DBname)
+	err(databaseError)
+	defer db.Close()
+	queryError := db.QueryRow(query, data).Scan(&data)
+	if queryError != nil {
+		if queryError != sql.ErrNoRows {
+			panic(err)
+		}
+		return false
+	}
+	return true
 }
